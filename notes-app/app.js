81abe03789226b39e44d26ef0ec7cd09bd52c378
Carlos <carlos.goncalves@agrofauna.com.br>
2022-03-14 13:43:10 -0300

    Notes App Versao 0.7 npm packages gitIgnore and validator Setup

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
-
 console.log('Nota: ', msg)
+
+console.log(validator.isEmail('carlos@example.com'))
