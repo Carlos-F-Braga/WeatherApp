18049e94165581e65532316b4ecbf4aba5b065cc
Carlos <carlos.goncalves@agrofauna.com.br>
2022-03-14 13:30:04 -0300

    Notes App Versao 0.6 Notes.js exports

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
 const add = require('./utils.js')
+const getNotes = require('./notes.js')
 
 const sum = add(4, -2)
+const msg = getNotes()
 
-console.log(sum)
+console.log('Soma: ', sum)
+console.log('Nota: ', msg)
