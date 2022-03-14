d8ce47032b97565db0c9fbcb02758a77d72c48ca
Carlos <carlos.goncalves@agrofauna.com.br>
2022-03-14 13:13:04 -0300

    Versao 0.3 fs appendFileSync

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My name is Carlos Frederyco.')
+//fs.writeFileSync('notes.txt', 'My name is Carlos Frederyco.')
+
+fs.appendFileSync('notes.txt', 'I live in Sao Jose do Rio Preto')
