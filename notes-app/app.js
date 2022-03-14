4720fe23933fae149b69aa8971d6bba513945357
Carlos <carlos.goncalves@agrofauna.com.br>
2022-03-14 14:15:25 -0300

    Notes App Versao 0.8 Chalk Require completed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,7 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 console.log('Nota: ', msg)
+console.log(chalk.green("Sucesso"))
 
-console.log(validator.isURL('https://mead.io'))
-console.log(validator.isEmail('Carlos@Exemplo.com'))
