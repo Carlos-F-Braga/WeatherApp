ecadfbe08a3e40b2a1d56312374368047b9d5d2f
Carlos <carlos.goncalves@agrofauna.com.br>
2022-04-12 09:59:49 -0300

    Notes App Versao 1.2 Yargs and its Commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,46 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Adiciona uma nova Nota',
+    handler: function () {
+        console.log('Adicionando uma nova Nota!')
+    }    
+})
 
-if (command === 'add') {
-    console.log('Adding note!')
-}
-else if (command === 'remove') {
-    console.log('Removing note!')
-}
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove uma Nota',
+    handler: function () {
+        console.log('Removendo uma Nota!')
+    } 
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Lista as Notas',
+    handler: function () {
+        console.log('Listando todas as Notas!')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Le uma Nota',
+    handler: function () {
+        console.log('Lendo uma Nota!')
+    }
+})
+
+//add, remove, read, list
+
+console.log(yargs.argv)
