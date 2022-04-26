2f132cfbdc2d84a4fe10fd478230147abdccb28e
Carlos <carlos.goncalves@agrofauna.com.br>
2022-04-26 10:42:58 -0300

    Weather App Versao 1.02 Specific Http Console log Request Weather Descriptiomn

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,10 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=1372f2db0562c13bc01b59b2830654a9&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=1372f2db0562c13bc01b59b2830654a9&query=37.8267,-122.4233&units=m'
 
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.current)
     
+    console.log(response.body.current.weather_descriptions[0])
     console.log('Atualmente a temperatura é de ' + response.body.current.temperature + ' graus, mas parece ser de ' + response.body.current.feelslike + ' graus' )
 })
