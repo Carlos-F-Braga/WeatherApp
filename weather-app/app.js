a71ad2a7d8b9f3bd2b7cb564b15d6795326cd98b
Carlos Frederyco <carlos.goncalves@agrofauna.com.br>
2022-05-01 20:22:07 -0300

    Weather App Versao 1.6 Dinamic Location Geocoding and Forecasting finished

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,28 @@
-const request = require('request')
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
+const address = process.argv[2]
 
-forecast(37.8267, -122.4233, (error, data) => {
-    console.log('Fore Erro: ', error)
-    console.log('Fore Data: ', data)
-  })
 
-geocode('Philadelphia', (error, data) => {
-    console.log('Geo Erro: ', error)
-    console.log('Geo Data: ', data)
+if (address){
+console.log('Lugar passado:', address)
+geocode(address, (error, data) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error){
+            return console.log(error)
+        }
+
+        console.log('Lugar: ', data.lugar)
+        console.log('Data: ', forecastData)
+      })
 })
+}
+else {
+    console.log('Indique o Local que deseja realizar a Previsão Climática!')
+}
 
 //console.log(a(2, 3))
