0554becfbba544aad570d192c9550384a26399db
Carlos Frederyco <carlos.goncalves@agrofauna.com.br>
2022-05-01 21:31:11 -0300

    Weather App Versao 2.1 Html and JSON response send

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1> Weather </h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help Page!')
+    res.send([{
+        name: 'Andrew',
+        age: 27
+    }, {
+        name: 'Carlos',
+        age: 20
+    }])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page!')
+    res.send('<h1> About! </h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page!')
+    res.send([{
+        Location: 'Philadelphia',
+        Forecast: 'Está Nevando'
+    }, {
+        Location: 'Rio Preto',
+        Forecast: 'Está Queimando'
+    }])
 })
 
 const port = 3000
