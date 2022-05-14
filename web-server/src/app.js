79ff44dbe232958b42c8947549e0fdbda9633408
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-05-14 18:21:35 -0300

    Hbs Views Programmed and Logic

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const PublicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(PublicDirectoryPath))
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Carlos Frederyco'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Carlos Frederyco'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
