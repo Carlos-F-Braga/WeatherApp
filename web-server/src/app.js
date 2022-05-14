7bb930efe08ebd2d6c0dc27ee769e59146cfe6fe
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-05-14 19:03:44 -0300

    Header Footer Css Programming

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+const carlos = 'Carlos Frederyco'
+
 // Define Paths for Express config
 const PublicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 //Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(PublicDirectoryPath))
@@ -17,20 +22,22 @@ app.use(express.static(PublicDirectoryPath))
 app.get('', (req,res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Carlos Frederyco'
+        name: carlos
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About me',
-        name: 'Carlos Frederyco'
+        name: carlos
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text'
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: carlos
     })
 })
 
