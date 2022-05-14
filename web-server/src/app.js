9c214744507e11e95f146c3bf1ad0de7637c3f96
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-05-14 17:44:39 -0300

    Express Static Path - About and Help Pages Created

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,10 @@
+const path = require('path');
 const express = require('express')
 
 const app = express()
+const PublicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('<h1> Weather </h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Andrew',
-        age: 27
-    }, {
-        name: 'Carlos',
-        age: 20
-    }])
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1> About! </h1>')
-})
+app.use(express.static(PublicDirectoryPath))
 
 app.get('/weather', (req, res) => {
     res.send([{
