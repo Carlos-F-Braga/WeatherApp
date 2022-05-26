7b82ef485dab674a7a19a00efca26f0ad4e0a516
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-05-26 20:38:33 -0300

    Browser Http Requests Fetch Programming for app.js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,19 @@
 console.log('Client side javaScript file is loaded!')
+
+fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        }
+        else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
