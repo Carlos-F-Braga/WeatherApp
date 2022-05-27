6b3ac72febb1ea8ade8f2a48deffba253ad7318f
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-05-26 21:27:03 -0300

    Javascript querySelect Programmed and Css Finished

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,15 +1,28 @@
 console.log('Client side javaScript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error)
+            messageOne.textContent = 'Digite um Endereço!'
         }
         else {
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     })
+})
 })
 
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
